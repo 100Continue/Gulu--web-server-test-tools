c58487a39b684547cb2fd3e153a162cb7ab74751
lingj <lingj1992@163.com>
2018-03-16 16:17:55 +0800

    1、新增testNG重试机制，用例运行失败默认重试3次； 2、新增testng.xml，支持多线程并发测试； 3、新增testNg Report报告功能并美化报告模板展示；

---
+++ src/test/java/Base/TestListener.java
@@ -0,0 +1,55 @@
+package Base;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestNGMethod;
+import org.testng.ITestResult;
+
+import java.util.Iterator;
+
+public class TestListener implements ITestListener {
+
+	public static final String CONFIG = null;
+
+	@Override
+	public void onFinish(ITestContext context) {
+		Iterator<ITestResult> listOfFailedTests = context.getFailedTests().getAllResults().iterator();
+		while (listOfFailedTests.hasNext()) {
+			ITestResult failedTest = listOfFailedTests.next();
+			ITestNGMethod method = failedTest.getMethod();
+			if (context.getFailedTests().getResults(method).size() > 1) {
+				listOfFailedTests.remove();
+			} else {
+				if (context.getPassedTests().getResults(method).size() > 0) {
+					listOfFailedTests.remove();
+				}
+			}
+		}
+	}
+
+	// Following are all the method stubs that you do not have to implement
+
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+	}
+
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+	}
+
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+	}
+
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+	}
+} // ends the class
